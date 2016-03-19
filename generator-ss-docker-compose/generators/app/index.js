e111b502b9b5b21350a24c1d1f8e03e21666967c
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-03-19 15:53:55 +0900

    *

--- generator-ss-docker-compose/generators/app/index.js
+++ generator-ss-docker-compose/generators/app/index.js
@@ -1,40 +1,11 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
-module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the super ' + chalk.red('') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
+module.exports = yeoman.Base.extend({
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.directory(
+      this.templatePath('.'),
+      this.destinationPath('.')
     );
-  },
-
-  install: function () {
-    this.installDependencies();
   }
 });
